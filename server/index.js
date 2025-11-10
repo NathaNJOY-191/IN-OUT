f81d530d58ecfea0c4dc4f4cda2bf58e5fd0274f
nathanjoy911@gmail.com <nathanjoy911@gmail.com>
2025-11-10 07:36:39 +0530

    Update CORS configuration

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const jwt = require('jsonwebtoken');
 const Razorpay = require('razorpay');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://in-out-iota.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Basic root route so visiting / in a browser doesn't show "Cannot GET /"
